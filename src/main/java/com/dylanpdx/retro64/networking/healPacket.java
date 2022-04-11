d27346599abf07edc0c918065ef036806de9c1dd
min1up <supasugachris@live.com>
2022-04-11 00:40:59 -0600

    Update healPacket.java

--- src/main/java/com/dylanpdx/retro64/networking/healPacket.java
+++ src/main/java/com/dylanpdx/retro64/networking/healPacket.java
@@ -1,6 +1,6 @@
 package com.dylanpdx.retro64.networking;
 
-import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.PacketByteBuf;
 
 import java.util.function.BiConsumer;
 import java.util.function.Function;
@@ -9,32 +9,32 @@ import java.util.function.Function;
  * Heal packet - Syncs when a player should heal
  */
 public class healPacket {
-
+    
     byte healAmount;
 
-    public static final BiConsumer<healPacket, FriendlyByteBuf> encoder = new BiConsumer<healPacket, FriendlyByteBuf>() {
+    public static final BiConsumer<healPacket, PacketByteBuf> encoder = new BiConsumer<healPacket, PacketByteBuf>() {
         @Override
-        public void accept(healPacket dmgPacket, FriendlyByteBuf friendlyByteBuf) {
+        public void accept(healPacket dmgPacket, PacketByteBuf friendlyByteBuf) {
             friendlyByteBuf.writeByte(dmgPacket.healAmount);
         }
     };
 
-
-    public static Function<FriendlyByteBuf, healPacket> decoder = new Function<FriendlyByteBuf, healPacket>() {
+    public static Function<PacketByteBuf, healPacket> decoder = new Function<PacketByteBuf, healPacket>() {
         @Override
-        public healPacket apply(FriendlyByteBuf friendlyByteBuf) {
+        public healPacket apply(PacketByteBuf friendlyByteBuf) {
             healPacket dmgPacket = new healPacket();
             dmgPacket.healAmount = friendlyByteBuf.readByte();
             return dmgPacket;
         }
     };
 
-    private healPacket(){
+    private healPacket() {
         healAmount = 0;
     }
 
-    public healPacket(byte healAmount){
+    public healPacket(byte healAmount) {
         this.healAmount = healAmount;
     }
 
 }
+
