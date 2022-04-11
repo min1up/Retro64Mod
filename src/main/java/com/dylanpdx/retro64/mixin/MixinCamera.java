a70731052aca1fbe8850ec9c16fdd41c89c18044
min1up <supasugachris@live.com>
2022-04-10 23:48:16 -0600

    Update MixinCamera.java

--- src/main/java/com/dylanpdx/retro64/mixin/MixinCamera.java
+++ src/main/java/com/dylanpdx/retro64/mixin/MixinCamera.java
@@ -1,8 +1,8 @@
 package com.dylanpdx.retro64.mixin;
 
 import com.dylanpdx.retro64.RemoteMCharHandler;
-import net.minecraft.client.Camera;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.Camera;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,7 +13,7 @@ public class MixinCamera {
 
     @Inject(at=@At("HEAD"), method="Lnet/minecraft/client/Camera;getMaxZoom(D)D", cancellable = true)
     private void getMaxZoom(double pStartingDistance, CallbackInfoReturnable<Double> callback){
-        if (RemoteMCharHandler.getIsMChar(Minecraft.getInstance().player))
+        if (RemoteMCharHandler.getIsMChar(MinecraftClient.getInstance().player))
             callback.setReturnValue(pStartingDistance);
     }
 }
