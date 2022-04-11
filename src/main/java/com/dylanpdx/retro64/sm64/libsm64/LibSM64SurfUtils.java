f8b6a9906b2046bdec4980317f4f1af187eb5d4e
min1up <supasugachris@live.com>
2022-04-11 00:03:41 -0600

    Update LibSM64SurfUtils.java

--- src/main/java/com/dylanpdx/retro64/sm64/libsm64/LibSM64SurfUtils.java
+++ src/main/java/com/dylanpdx/retro64/sm64/libsm64/LibSM64SurfUtils.java
@@ -1,16 +1,16 @@
 package com.dylanpdx.retro64.sm64.libsm64;
 
 import com.dylanpdx.retro64.sm64.SM64SurfaceType;
-import com.mojang.math.Vector3f;
-import net.minecraft.world.phys.Vec3;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3f;
 
 public class LibSM64SurfUtils {
-    public static SM64Surface generateTri(Vector3f point1, Vector3f point2, Vector3f point3, Vector3f translation, short surface, short terrain)
+    public static SM64Surface generateTri(Vec3f point1, Vec3f point2, Vec3f point3, Vec3f translation, short surface, short terrain)
     {
-        //translation = translation + new Vector3f(0, -1, 0);
-        Vector3f cp1 = point1.copy();
-        Vector3f cp2 = point2.copy();
-        Vector3f cp3 = point3.copy();
+        //translation = translation + new Vec3f(0, -1, 0);
+        Vec3f cp1 = point1.copy();
+        Vec3f cp2 = point2.copy();
+        Vec3f cp3 = point3.copy();
         cp1.add(translation);
         cp2.add(translation);
         cp3.add(translation);
@@ -23,21 +23,21 @@ public class LibSM64SurfUtils {
         surf.type = surface;
         surf.terrain = terrain;
         // tri 1
-        surf.vertices[0] = (int)cp1.x();
-        surf.vertices[1] = (int)cp1.y();
-        surf.vertices[2] = (int)cp1.z();
+        surf.vertices[0] = (int)cp1.getX();
+        surf.vertices[1] = (int)cp1.getY();
+        surf.vertices[2] = (int)cp1.getZ();
         // tri 2
-        surf.vertices[3] = (int)cp2.x();
-        surf.vertices[4] = (int)cp2.y();
-        surf.vertices[5] = (int)cp2.z();
+        surf.vertices[3] = (int)cp2.getX();
+        surf.vertices[4] = (int)cp2.getY();
+        surf.vertices[5] = (int)cp2.getZ();
         // tri 3
-        surf.vertices[6] = (int)cp3.x();
-        surf.vertices[7] = (int)cp3.y();
-        surf.vertices[8] = (int)cp3.z();
+        surf.vertices[6] = (int)cp3.getX();
+        surf.vertices[7] = (int)cp3.getY();
+        surf.vertices[8] = (int)cp3.getZ();
         return surf;
     }
 
-    public static SM64Surface[] generateQuad(Vector3f a, Vector3f b, Vector3f c, Vector3f d, Vector3f translation, short surface, short terrain)
+    public static SM64Surface[] generateQuad(Vec3f a, Vec3f b, Vec3f c, Vec3f d, Vec3f translation, short surface, short terrain)
     {
         return new SM64Surface[]
                 {
@@ -46,7 +46,7 @@ public class LibSM64SurfUtils {
                 };
     }
 
-    public static SM64Surface[] generateQuadF(Vector3f a, Vector3f b, Vector3f c, Vector3f d, Vector3f translation, short surface, short terrain)
+    public static SM64Surface[] generateQuadF(Vec3f a, Vec3f b, Vec3f c, Vec3f d, Vec3f translation, short surface, short terrain)
     {
         return new SM64Surface[]
                 {
@@ -59,18 +59,18 @@ public class LibSM64SurfUtils {
         return block(x,y,z,1.1f,-0.1f,(short) SM64SurfaceType.Default,(short)0);
     }
 
-    public static Vec3[] surfToVec(SM64Surface surf){
-        Vec3[] ret = new Vec3[3];
-        ret[0] = new Vec3(surf.vertices[0],surf.vertices[1],surf.vertices[2]);
-        ret[1] = new Vec3(surf.vertices[3],surf.vertices[4],surf.vertices[5]);
-        ret[2] = new Vec3(surf.vertices[6],surf.vertices[7],surf.vertices[8]);
+    public static Vec3d[] surfToVec(SM64Surface surf){
+        Vec3d[] ret = new Vec3d[3];
+        ret[0] = new Vec3d(surf.vertices[0],surf.vertices[1],surf.vertices[2]);
+        ret[1] = new Vec3d(surf.vertices[3],surf.vertices[4],surf.vertices[5]);
+        ret[2] = new Vec3d(surf.vertices[6],surf.vertices[7],surf.vertices[8]);
         return ret;
     }
 
-    public static Vec3[] surfsToVec(SM64Surface[] surfs){
-        Vec3[] ret = new Vec3[surfs.length*3];
+    public static Vec3d[] surfsToVec(SM64Surface[] surfs){
+        Vec3d[] ret = new Vec3d[surfs.length*3];
         for(int i=0;i<surfs.length;i++){
-            Vec3[] temp = surfToVec(surfs[i]);
+            Vec3d[] temp = surfToVec(surfs[i]);
             ret[i*3] = temp[0];
             ret[i*3+1] = temp[1];
             ret[i*3+2] = temp[2];
@@ -80,14 +80,14 @@ public class LibSM64SurfUtils {
 
     public static SM64Surface[] block(int x, int y, int z, float blockTop, float blockBottom, short surface, short terrain)
     {
-        var topQuad = generateQuadF(new Vector3f(0, blockTop, 0), new Vector3f(1, blockTop, 0), new Vector3f(1, blockTop, 1), new Vector3f(0, blockTop, 1), new Vector3f(z, y, x),surface,terrain);
-        var bottomQuad = generateQuad(new Vector3f(0, blockBottom, 0), new Vector3f(1, blockBottom, 0), new Vector3f(1, blockBottom, 1), new Vector3f(0, blockBottom, 1), new Vector3f(z, y, x), surface, terrain);
+        var topQuad = generateQuadF(new Vec3f(0, blockTop, 0), new Vec3f(1, blockTop, 0), new Vec3f(1, blockTop, 1), new Vec3f(0, blockTop, 1), new Vec3f(z, y, x),surface,terrain);
+        var bottomQuad = generateQuad(new Vec3f(0, blockBottom, 0), new Vec3f(1, blockBottom, 0), new Vec3f(1, blockBottom, 1), new Vec3f(0, blockBottom, 1), new Vec3f(z, y, x), surface, terrain);
 
-        var sideAQuad = generateQuadF(new Vector3f(0, blockBottom, 0), new Vector3f(1, blockBottom, 0), new Vector3f(1, blockTop, 0), new Vector3f(0, blockTop, 0), new Vector3f(z, y, x), surface, terrain);
-        var sideBQuad = generateQuadF(new Vector3f(0, blockBottom, 1), new Vector3f(0, blockBottom, 0), new Vector3f(0, blockTop, 0), new Vector3f(0, blockTop, 1), new Vector3f(z, y, x), surface, terrain);
+        var sideAQuad = generateQuadF(new Vec3f(0, blockBottom, 0), new Vec3f(1, blockBottom, 0), new Vec3f(1, blockTop, 0), new Vec3f(0, blockTop, 0), new Vec3f(z, y, x), surface, terrain);
+        var sideBQuad = generateQuadF(new Vec3f(0, blockBottom, 1), new Vec3f(0, blockBottom, 0), new Vec3f(0, blockTop, 0), new Vec3f(0, blockTop, 1), new Vec3f(z, y, x), surface, terrain);
 
-        var sideCQuad = generateQuadF(new Vector3f(1, blockBottom, 1), new Vector3f(0, blockBottom, 1), new Vector3f(0, blockTop, 1), new Vector3f(1, blockTop, 1), new Vector3f(z, y, x), surface, terrain);
-        var sideDQuad = generateQuadF(new Vector3f(1, blockBottom, 0), new Vector3f(1, blockBottom, 1), new Vector3f(1, blockTop, 1), new Vector3f(1, blockTop, 0), new Vector3f(z, y, x), surface, terrain);
+        var sideCQuad = generateQuadF(new Vec3f(1, blockBottom, 1), new Vec3f(0, blockBottom, 1), new Vec3f(0, blockTop, 1), new Vec3f(1, blockTop, 1), new Vec3f(z, y, x), surface, terrain);
+        var sideDQuad = generateQuadF(new Vec3f(1, blockBottom, 0), new Vec3f(1, blockBottom, 1), new Vec3f(1, blockTop, 1), new Vec3f(1, blockTop, 0), new Vec3f(z, y, x), surface, terrain);
         SM64Surface[] surfs = new SM64Surface[]
                 {
                         // top
@@ -116,7 +116,7 @@ public class LibSM64SurfUtils {
 
     public static SM64Surface[] plane(int x, int y, int z, float blockBottom, short surface, short terrain)
     {
-        var bottomQuad = generateQuadF(new Vector3f(0, blockBottom, 0), new Vector3f(1, blockBottom, 0), new Vector3f(1, blockBottom, 1), new Vector3f(0, blockBottom, 1), new Vector3f(z, y, x), surface, terrain);
+        var bottomQuad = generateQuadF(new Vec3f(0, blockBottom, 0), new Vec3f(1, blockBottom, 0), new Vec3f(1, blockBottom, 1), new Vec3f(0, blockBottom, 1), new Vec3f(z, y, x), surface, terrain);
 
 
         return new SM64Surface[]
